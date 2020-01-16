a60397b4954b0d578d5a7f309a32cd69e8da1e23
zhaopeilin <79034@topscore.cn>
2020-01-16 18:16:25 +0800

    leetcode

--- leetcode/leetcode299/leetcode299.go
+++ leetcode/leetcode299/leetcode299.go
@@ -1,1 +1,12 @@
 package leetcode299
+
+func getHint(secret string, guess string) string {
+	//位置和数字都对，给一个A，位置不对数字对，给一个B
+	list1 := []byte(secret)
+	list2 := []byte(guess)
+
+	for i := 0; i < len(list1); i++ {
+		if list1[i]==list2[i]
+
+	}
+}
--- leetcode/leetcode299/leetcode299_test.go
+++ leetcode/leetcode299/leetcode299_test.go
@@ -2,6 +2,21 @@ package leetcode299
 
 import "testing"
 
+var data = []struct {
+	in1 string
+	in2 string
+	out string
+}{
+	{"1807", "7810", "1A3B"},
+	{"1123", "0111", "1A1B"},
+}
+
 func TestAll(t *testing.T) {
+	for _, d := range data {
+		r := getHint(d.in1, d.in2)
+		if r != d.out {
+			t.Error(d.out, r)
+		}
+	}
 
 }
