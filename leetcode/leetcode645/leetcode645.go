333ea910aeb3e781a6bc2518f38326c6fad39f4a
zhaopeilin <284340211@qq.com>
2020-01-17 21:25:57 +0800

    leetcode

--- leetcode/leetcode645/leetcode645.go
+++ leetcode/leetcode645/leetcode645.go
@@ -3,7 +3,19 @@ package leetcode645
 func findErrorNums(nums []int) []int {
 	//返回重复的数字，然后是缺失的数字
 	// [2, 10000] 1到 n 的整数
+	m := make(map[int]bool)
 	l := len(nums)
+	var d int
+	acc := 0
 	sum := l * (l + 1) / 2
-
+	for _, num := range nums {
+		acc += num
+		_, exists := m[num]
+		if exists {
+			d = num
+		} else {
+			m[num] = true
+		}
+	}
+	return []int{d, sum + d - acc}
 }
